ec9a661fc8e7f6d41d9914840f74a37c566fd086
kwidua <kwidua@gmail.com>
2026-02-09 10:52:54 +0100

    Change title

--- src/components/Pets.jsx
+++ src/components/Pets.jsx
@@ -7,7 +7,7 @@ export default function Pets() {
     return (
         <>
         <section className="text-center">
-        <h2 className="m-4">Meine Haustiere</h2>
+        <h2 className="m-4">Mitarbeiter</h2>
         <div className="inline-flex flex-wrap gap-4">
         <ProfileImage url={Siggi} variables={"sm:opacity-55 hover:opacity-100 lg:w-100 lg:h-100 w-50 h-50"}/>
         <ProfileImage url={Lissi} variables={"sm:opacity-55 hover:opacity-100 lg:w-100 lg:h-100 w-50 h-50"} />
