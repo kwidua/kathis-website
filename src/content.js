7de89a84e20e5aed6827d7bbe21110ad2749007b
kwidua <kwidua@gmail.com>
2026-02-16 14:05:45 +0100

    add a content.js to extract content into its own file for a better overview

--- src/content.js
+++ src/content.js
@@ -1,10 +1,60 @@
 export const content = {
 hero: {
-    title1: "Some text",
-    text: "nngdgnnfgng",
+    title1: "Hi. I'm Kathi. Softwareengineer",
+    text: "8 Jahre Erfahrung in PHP/Symfony. Aktuell vertiefe ich meine Expertise in React und moderner Frontend-Architektur.",
 },
 
-about: {
+skills: {
+    technical: [
+    ]
+},
 
+lebenslauf: {
+    steps: [
+        {
+            date: 'Feb 2026',
+            title: 'Weiterbildung Syntax',
+            subtitle: 'Frontend mit React'
+        },
+        {
+            date: 'Mai, 2021',
+            title: 'Bachelor of Science',
+            subtitle: 'Informatik, Hochschule Bonn-Rhein-Sieg'
+        },
+        {
+            date: 'Okt, 2020',
+            title: 'Softwareengineer Backend',
+            subtitle: 'Tideways GmbH'
+        },
+        {
+            date: 'Aug, 2018',
+            title: 'Auslandssemester',
+            subtitle: 'Toronto, Kanada'
+        },
+        {
+            date: 'Okt, 2016',
+            title: 'Werkstudent Webentwicklung',
+            subtitle: 'Tideways GmbH'
+        },
+    ]  
+},
+
+projects: {
+    best: [
+        {
+            title: 'Eigene Website',
+            subtext: 'Developer Website',
+            link: 'https://github.com/kwidua/kathis-website'
+        },
+        {
+            title: 'Mandrake',
+            subtext: 'Kleine Web App zum einpflegen von Pflanzen fdhu dfshu whi',
+            link: ''
+        },
+        {
+            title: 'Eigene Website',
+            subtext: 'Developer Website'
+        }
+    ]
 }
 }
