4a86733eb2da7078ec08f1c2c883ed3bad1b248a
kwidua <kwidua@gmail.com>
2026-02-16 14:08:57 +0100

    add ui component button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,7 +5,7 @@ const variants = {
 
 export default function Button({onClick, children, variant}) {
     return (
-    <button className="" onClick={onClick}>
+    <button className="px-4 py-2 rounded-2xl bg-cyan-950 hover:bg-cyan-800" onClick={onClick}>
         {children}
     </button>
     )
